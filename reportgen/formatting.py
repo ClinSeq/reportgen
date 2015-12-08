7ff9c540e2e103ef606f751372a7752b5158ed71
Thomas Whitington <thowhi@130-229-9-128-dhcp.wlan.ki.se>
2015-12-08 15:00:00 +0100

    Implemented DocumentFormat class.

--- reportgen/formatting.py
+++ reportgen/formatting.py
@@ -7,12 +7,85 @@ Created on Dec 1, 2015
 
 class DocumentFormat(object):
     '''
-    classdocs
+    Document formatting options, initially intended for specifying the format
+    of genomic report latex documents.
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, checked, unchecked, fontfamily="Roman", fontsize="10pt",
+                 tablepos="left", language="Swedish"):
         '''
-        Constructor
+        Raises a ValueError if any of the specified option values are invalid.
+
+        checked: string specifying file location
+        unchecked: string specifying file location
+        fontfamily: Roman/SansSerif
+        fontsize: latex font size specifier string
+        tablepos: left/center
+        language: Swedish/English
         '''
-        
+
+        self._checked = checked
+        self._unchecked = unchecked
+        if not fontfamily in ["Roman", "SansSerif"]:
+            raise ValueError("Invalid font family for DocumenFormat: " + fontfamily)
+        self._fontfamily = fontfamily
+        self._fontsize = fontsize
+        if not tablepos in ["left", "center"]:
+            raise ValueError("Invalid table position for DocumenFormat: " + tablepos)
+        self._tablepos = tablepos
+        if not language in ["Swedish", "English"]:
+            raise ValueError("Invalid language for DocumenFormat: " + language)
+        self._tablepos = tablepos
+
+    def get_checked_checkbox(self):
+        return self._checked
+
+    def get_unchecked(self):
+        return self._unchecked
+
+    def get_fontfamily(self):
+        return self._fontfamily
+
+    def get_fontsize(self):
+        return self._fontsize
+
+    def get_table_pos(self):
+        return self._tablepos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
