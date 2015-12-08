ab48382b52c46cf3b887d472d75c7290363d693b
Thomas Whitington <thowhi@130-229-9-128-dhcp.wlan.ki.se>
2015-12-08 13:58:23 +0100

    Implementing switches and flags to main script.

--- reportgen/__main__.py
+++ reportgen/__main__.py
@@ -4,7 +4,7 @@ Created on Dec 1, 2015
 @author: thowhi
 '''
 
-import json, pdb, subprocess, sys, tempfile
+import json, os, pdb, subprocess, sys, tempfile
 from optparse import OptionParser
 import reports
 import formatting
@@ -20,17 +20,41 @@ FIXME: Need to agree on a structure for these files and document this somewhere
 and then link to that documentation here
 
 Outputs:
-- 
+- Generates a pdf file displaying the formatted report
 """
 
     parser = OptionParser(usage = description)
     parser.add_option("--boolFlag", action="store_true", dest="boolFlag",
                       default = False,
                       help = "")
-    parser.add_option("--stringSwitch", dest = "stringSwitch",
-                      default = "",
-                      help = "" + \
-                          ". Default=[%default]")
+    parser.add_option("--language", dest = "language",
+                      default = "Swedish",
+                      help = "Language in which the report text will be " + \
+                          "generated. One of Swedish or English. Default=[%default]")
+    parser.add_option("--fontfamily", dest = "fontfamily",
+                      default = "Roman",
+                      help = "Font in which the report text will be " + \
+                          "generated. One of Roman or SansSerif. Default=[%default]")
+    parser.add_option("--fontsize", dest = "fontsize",
+                      default = "10pt",
+                      help = "Font size for report text. " + \
+                          "Default=[%default]")
+    parser.add_option("--tablepos", dest = "tablepos",
+                      default = "left",
+                      help = "Position of tables in the document. " + \
+                          "One of left or center. Default=[%default]")
+    parser.add_option("--checked_checkbox", dest = "checked",
+                      default = os.path.abspath(os.path.dirname(__file__) + "/../rsz_checked_checkbox.png"),
+                      help = "png file for checked checkbox. " + \
+                          "Default=[%default]")
+    parser.add_option("--unchecked_checkbox", dest = "unchecked",
+                      default = os.path.abspath(os.path.dirname(__file__) + "/../rsz_unchecked_checkbox.png"),
+                      help = "png file for checked checkbox. " + \
+                          "Default=[%default]")
+    parser.add_option("--tmp_dir", dest = "tmp_dir",
+                      default = "/tmp",
+                      help = "Folder for containing temporary files. " + \
+                          "Default=[%default]")
     parser.add_option("--debug", action="store_true", dest="debug",
                       help = "Debug the program using pdb.")
     (options, args) = parser.parse_args()
@@ -81,27 +105,28 @@ Outputs:
         print >> sys.stderr, e
         sys.exit(1)
 
-    doc_format = formatting.DocumentFormat() # XXX Add arguments to constructor call
-    alassca_report = reports.AlasscaReport(meta_json, report_json, doc_format)
+    #doc_format = formatting.DocumentFormat() # XXX Add arguments to constructor call
+    #alassca_report = reports.AlasscaReport(meta_json, report_json, doc_format)
     
     # Generate a string of latex code representing the report:
-    report_latex_string = alassca_report.make_latex()
+    #report_latex_string = alassca_report.make_latex()
 
     # Write the resulting latex string to a temporary output file:
-    (tmp_latex_file, tmp_latex_filename) = tempfile.mkstemp("tmp", "Metadata", options.temp_dir)
-    print >> tmp_latex_file, report_latex_string
+    (tmp_latex_file, tmp_latex_filename) = tempfile.mkstemp("tmp", "Metadata", options.tmp_dir)
+    pdb.set_trace()
+    #print >> tmp_latex_file, report_latex_string
     tmp_latex_file.close()
     
     # Convert latex to pdf by running pdflatex on the command line:
-    call_result = subprocess.call(["pdflatex", tmp_latex_filename])
+    #call_result = subprocess.call(["pdflatex", tmp_latex_filename])
 
     # Delete the temporary latex file unless told to retain it in the command line options:
-    if not options.keep_latex:
-        subprocess.call(["rm", tmp_latex_filename])
+    #if not options.keep_latex:
+    #    subprocess.call(["rm", tmp_latex_filename])
     
-    if call_result != 0:
-        sys.stderr, "ERROR: pdflatex conversion failed."
-        sys.exit(1)
+    #if call_result != 0:
+    #    sys.stderr, "ERROR: pdflatex conversion failed."
+    #    sys.exit(1)
 
 
 if __name__ == '__main__':
