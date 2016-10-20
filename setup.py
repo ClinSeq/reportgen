8bc23bdbdd4ae716db7f9a8278b0c5e2565b005d
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-10-20 15:18:56 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
 
 setup(name='reportgen',
-      version='0.1.0',
+      version='0.1.1',
       packages=['reportgen', 'reportgen.reporting', 'reportgen.rules'],
       install_requires=reqs,
       package_data={'': ['assets/ALASCCA_MUTATION_TABLE_SPECIFIC.xlsx', 'assets/COLORECTAL_MUTATION_TABLE.xlsx',
