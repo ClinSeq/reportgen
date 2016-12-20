b6d6bb815054ed18c71fcf7078adf0dbb6f99dcc
Thomas Whitington <thowhi@LM0226MEB.local>
2016-12-20 15:20:03 +0100

    Implementing Caveat class.

--- reportgen/rules/caveats.py
+++ reportgen/rules/caveats.py
@@ -1,5 +1,24 @@
 from reportgen.reporting.features import PurityReport
 
+
+class Caveat:
+    UNCHANGED = "UNCHANGED"
+    NON_POSITIVE_TO_EB = "NON_POSITIVE_TO_EB"
+    ALL_TO_EB = "ALL_TO_EB"
+
+    def __init__(self):
+        self._action = None
+
+
+class CoverageInfo(Caveat):
+    def __init__(self, multi_qc_zip_file):
+        super(CoverageInfo, self).__init__()
+
+        # Extract coverage information from the MultiQC file...
+
+        # Set the self._action field according to the tumor/normal coverage values and the specified thresholds.
+
+
 class PurityRule:
     LOW_PURITY = "LOW"
 
