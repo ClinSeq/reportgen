cdbf40d2dd48d2ff0e7ffd2f9bce1098b3e00897
Thomas Whitington <thowhi@130-229-5-153-dhcp.wlan.ki.se>
2016-12-20 13:47:11 +0100

    New module caveats.py, relating to purity and contamination.

---
+++ reportgen/rules/caveats.py
@@ -0,0 +1,8 @@
+class PurityRule:
+    LOW_PURITY = "LOW"
+
+    def __init__(self, purity_dicts):
+        self._purity_ok = True
+        uniq_purity_calls = set(map(lambda dict: dict["purity.call"], purity_dicts))
+        if len(uniq_purity_calls) == 1 and uniq_purity_calls[0] == PurityRule.LOW_PURITY:
+            self._purity_ok = False
