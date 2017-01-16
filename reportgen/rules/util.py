b95b904c2bdcd07987edf6067d08fd05c38aed71
Thomas Whitington <thowhi@LM0226MEB.local>
2017-01-16 16:29:18 +0100

    Forgot None instantiations.

--- reportgen/rules/util.py
+++ reportgen/rules/util.py
@@ -16,12 +16,15 @@ def extract_qc_calls(purity_json, tumor_cov_json, normal_cov_json, contam_json):
     if not purity_json is None:
         purity_call = json.load(purity_json)["CALL"]
 
+    tumor_cov_call = None
     if not tumor_cov_json is None:
         tumor_cov_call = json.load(tumor_cov_json)["CALL"]
 
+    normal_cov_call = None
     if not normal_cov_json is None:
         normal_cov_call = json.load(normal_cov_json)["CALL"]
 
+    contam_call = None
     if not contam_json is None:
         contam_call = json.load(contam_json)["CALL"]
 
