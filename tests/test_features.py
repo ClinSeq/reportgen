f832b5724d88b5417ade21a478ef014bcc3424ac
Thomas Whitington <thowhi@LM0226MEB.local>
2017-01-18 10:46:12 +0100

    Boilerplate.

---
+++ tests/test_features.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from reportgen.reporting.features import *
+
+class TestAlasccaClassReport(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_XXX(self):
+        pass
+
