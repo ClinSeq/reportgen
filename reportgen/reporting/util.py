c14a08d6dca4402cff7f39dfa6351a9b2b1cf4fa
Thomas Whitington <thomaswhitington@gmail.com>
2017-04-03 14:44:22 +0200

    Keeping the personnummer formatting, we should use this soon.

--- reportgen/reporting/util.py
+++ reportgen/reporting/util.py
@@ -140,3 +140,8 @@ def parse_mutation_table(spreadsheet_filename):
         gene_symbol2classifications[symbol].append(curr_classification)
 
     return gene_symbol2classifications
+
+
+def format_personnummer(personnummer):
+    assert re.match("^[0-9]{12}$", personnummer)
+    return personnummer[2:8] + "-" + personnummer[8:]
