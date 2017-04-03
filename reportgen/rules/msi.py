7d66110f33b536702f2b53f59adef4b337a10282
Thomas Whitington <thomaswhitington@gmail.com>
2017-04-03 15:03:56 +0200

    Implementing the new MSI thresholding rules too (extra E.B. condition), since it's a small change.

--- reportgen/rules/msi.py
+++ reportgen/rules/msi.py
@@ -8,21 +8,23 @@ class MsiStatusRule:
     # FIXME: At some point I need to make these thresholds settable
     # somewhere. Currently hard-coding them here.
     MIN_TOTAL_SITES = 50
-    PERCENT_MSI_H = 5
+    PERCENT_MSI_LOW_THRESH = 5
+    PERCENT_MSI_HIGH_THRESH = 30
 
     def __init__(self, msi_status):
         self.msi_status = msi_status
 
     def apply(self):
 
-        status_string = None
         if self.msi_status.get_total() < self.MIN_TOTAL_SITES:
             status_string = FeatureStatus.NOT_DETERMINED
         else:
-            if self.msi_status.get_percent() > self.PERCENT_MSI_H:
+            if self.msi_status.get_percent() > self.PERCENT_MSI_HIGH_THRESH:
                 status_string = MsiReport.MSI
-            else:
+            elif self.msi_status.get_percent() < self.PERCENT_MSI_LOW_THRESH:
                 status_string = MsiReport.MSS
+            else:
+                status_string = FeatureStatus.NOT_DETERMINED
 
         report = MsiReport()
         report.msi_status = status_string
